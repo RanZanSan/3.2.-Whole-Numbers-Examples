af731dc9ab2efb1edcaaddef9ad4247cae024ccd
Andrey Rasskazov <ranzansan@gmail.com>
2021-05-21 15:15:25 +0300

    Добавьте файлы проекта.

---
+++ integerOperationsExamples/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace integerOperationsExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+            Console.WriteLine("a =  " + a + ", b = " + b);
+
+            int result = a + b;
+            Console.WriteLine("Сложение, a + b =  " + result);
+
+            result = a * b;
+            Console.WriteLine("Умножение, a * b =  " + result);
+
+            result = a / b;
+            Console.WriteLine("Деление, a / b =  " + result + " a и  b - целые числа, деление только нацело");
+
+            int resultMod = a % b;
+            Console.WriteLine("Деление с остатком, a % b =  " + resultMod + "Остаток от деления");
+
+            Console.ReadKey();
+
+            // При вычислении выражений, содержащих несколько операций, приоритет операций такой же как в математике.Чтобы повысить приоритет операции, используются скобки:
+            // 2 + 2 * 2 = 6
+            // (2 + 2) * 2 = 8
+        }
+    }
+}
---
+++ unaryOperatorsExamples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unaryOperatorsExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 5;
+            a++; // a = 1   
+            b--; // b = 4
+
+            Console.WriteLine(a + " " + b);
+            Console.ReadKey();
+        }
+    }
+}
